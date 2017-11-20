d0fe5ce80c71cc8bb93c882c11a2d36c2993815f
Yiran Huang <ihuangyiran@icloud.com>
2017-11-20 13:23:18 +0100

    1. add model basicLinear_dropout. 2. add questions

--- models/LinearModel.py
+++ models/LinearModel.py
@@ -4,15 +4,19 @@ import numpy
 
 class BasicLinear(torch.nn.Module):
     """
-    multi-layer linear model: 1000 - 500 - 1 
+    multi-layer linear model: 1000 - dim2 - dim3 - 1 
+    use batch normalization, problem is that each unit needs two more parameters. 
+    ?? should i add a bn for the firt layer for each batch input, other direct normalize all the input 
+    ?? because i have done the BN. Does that means i can delete the bias in linear model if so i can save some parameter.
+    ?? drop out has the function of Ensemble can it improve the result
     """
-    def __init__(self, dim2 = 500, dim3 = None, act_func = "ReLU", act_func_out = None, d_rate = 0.5, mom = 0.1):
+    def __init__(self, dim2 = 500, dim3 = None, act_func = "ReLU", act_func_out = None, mom = 0.1):
         super(BasicLinear, self).__init__()
         dim1 = 1000
         self.layers = torch.nn.Sequential()
         self.layers.add_module("fc1", torch.nn.Linear(dim1, dim2))
         self.layers.add_module("bn", torch.nn.BatchNorm1d(dim2))
-        self.layers.add_moudle(act_func + "1", getattr(torch.nn, act_func)())
+        self.layers.add_module(act_func + "1", getattr(torch.nn, act_func)())
  
         if dim3:
             self.layers.add_module("fc2", torch.nn.Linear(dim2, dim3))
@@ -21,12 +25,49 @@ class BasicLinear(torch.nn.Module):
 #            self.layers.add_module("drop_out", torch.nn.Dropout(d_rate))
             self.layers.add_module("fc3", torch.nn.Linear(dim3, 1))
         else:
- #           self.layers.add_module("drop_out", torch.nn.Dropout(d_rate))
+#            self.layers.add_module("drop_out", torch.nn.Dropout(d_rate))
             self.layers.add_module("fc2", torch.nn.Linear(dim2, 1))
         
         # 因为score的分数是从-1到1， 所以对应的结果是否加一个激活函数会比较好
         if act_func_out:
             self.layers.add_module("bn_out",  torch.nn.BatchNorm1d(dim3, momentum = mon))
+            self.layers.add_module(act_func_out, getattr(torch.nn, act_func_out)())
+
+    def forward(self, input):
+        """
+        input: (batch_size, 1000)
+        output: (1)
+        """
+        out = self.layers(input)
+        return out
+
+class BasicLinear_dropout(torch.nn.Module):
+    """
+    multi-layer linear model: 1000 - dim2 - dim3 - 1 
+    only use drop out 
+    i am not sure if it can convergence quickly without BN. 
+    try to normalize the input for the training. 
+    only use the activation function without core area.
+    use drop out to get a besser result.
+    """
+    def __init__(self, dim2 = 500, dim3 = None, act_func = "ReLU", act_func_out = None):
+        super(BasicLinear, self).__init__()
+        dim1 = 1000
+        self.layers = torch.nn.Sequential()
+        self.layers.add_module("fc1", torch.nn.Linear(dim1, dim2))
+        self.layers.add_module(act_func + "1", getattr(torch.nn, act_func)())
+ 
+        if dim3:
+            self.layers.add_module("fc2", torch.nn.Linear(dim2, dim3))
+            self.layers.add_module(act_func + "2", getattr(torch.nn, act_func)())
+            self.layers.add_module("drop_out", torch.nn.Dropout(d_rate))
+            self.layers.add_module("fc3", torch.nn.Linear(dim3, 1))
+        else:
+            self.layers.add_module("drop_out", torch.nn.Dropout(d_rate))
+            self.layers.add_module("fc2", torch.nn.Linear(dim2, 1))
+        
+        # 因为score的分数是从-1到1， 所以对应的结果是否加一个激活函数会比较好
+        if act_func_out:
             self.layers.add_module(act_func_out, getattr(torch.nn, act_func_out)())
 
     def forward(self, input):
