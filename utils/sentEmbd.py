d394b43bff9bd20b28fe1da3517575e662e25250
Yiran Huang <ihuangyiran@icloud.com>
2018-06-28 14:27:35 +0200

    add sentence embedding

---
+++ utils/sentEmbd.py
@@ -0,0 +1,136 @@
+#-*- coding: utf-8 -*-
+"""
+get sentencee embedding from word embeding
+"""
+import sys
+sys.path.append('./utils')
+import numpy as np
+import sklearn
+import torch
+
+from wordDict import WordDict
+from tqdm import tqdm
+from sklearn.decomposition import PCA
+
+class WR:
+    def __init__(self, path):
+        """
+        path: string or list of string 
+            the path of the dataset that used to create the dict or the list of sentence
+        """
+        # load dict
+        self.dict = WordDict(path)
+        self.num_dict= self.dict.get_num_dict()
+        self.freq_dict = self.dict.get_freq_dict()
+        self.alpha = 1e-3
+
+    def forward(self, sents, wordEs):
+        """
+        !!!false now, please copy the code from forward2 first
+        only work for nn hidden layer data, it has the following specialities:
+            1. start with a start sign
+            2. fix seq length, append with 0(???may be not)
+            3. all sentence length smaller as fix seq length
+        sents: string or list of string 
+            path of the file set or list of sentence
+        wordEs: string or numpy.matrix
+            path of the dataset, the data should be 
+            numpy matrix (batch_size, seq_len, num_dim) 
+            or is the data
+        """
+        seq_len = 100
+        # cal Vs
+        if type(wordEs) == str:
+            wordEs= np.load(wordEs)
+        else:
+            wordEs = wordEs
+        if type(sents) == str:
+            sents = [li.rstrip('\n').split(' ') for li in open(sents)] # load sentence
+        else:
+            sents = sents
+        assert(len(wordEs) == len(sents))
+        len_sents = torch.FloatTensor([len(li) for li in sents]) # get length of each sentense (batch_size)
+        sents = [li.insert(0, 'BOS') for li in sents] # add BOS sign
+        sents = [li.append('EOS') for li in sents] # add EOS sign
+        freq_sents = torch.FloatTensor([map(lambda x: self.dict.get_freq_of_words(x), sent) for sent in sents]) # get the freq
+        freq_sents.append(torch.FloatTensor(seq_len)) # should be the max length
+        padded_freq_sents = torch.nn.utils.rnn.pad_sequence(freq_sents, batch_fiost = True)[:-1].unsqueeze(1) # (batch_size, 1, seq_len)
+        pref = self.alpha/(self.alpha + padded_freq_sents)
+        unit = len_sents * torch.bmm(pref, wordEs) # (batch_size, num_dim)
+        # pca
+        unit = unit.numpy()
+        pca = PCA(unit.shape[1], svd_solver = 'auto')
+        pca.fit(unit)
+        out = unit - np.dot(np.dot(unit, pca.components_.T), pca.components_)
+        return out
+    
+    def forward2(self, sents_li, wordEs_li):
+        """
+        compare with forward, this function works when more then one dataset are gegeben 
+        doesn't accept the path
+        only work for nn hidden layer data, it has the following specialities:
+            1. start with a start sign
+            2. fix seq length, append with 0(???may be not)
+            3. all sentence length smaller as fix seq length
+        sents: string or list of string 
+            path of the file set or list of sentence
+        wordEs: string or numpy.matrix
+            path of the dataset, the data should be 
+            numpy matrix (batch_size, seq_len, num_dim) 
+            or is the data
+        """
+        seq_len = 100
+        assert(len(sents_li) == len(wordEs_li))
+        unit = []
+        for index in tqdm(range(len(sents_li))):
+            if type(sents_li[index]) == str:
+                sents = [li.rstrip('\n').split(' ') for li in open(sents_li[index])]
+            else:
+                sents = sents_li[index]
+                sents = [li.split(' ') for li in sents]
+            if type(wordEs_li[index]) == str:
+                wordEs= torch.from_numpy(np.load(wordEs_li[index]))
+            else:
+                wordEs = wordEs_li
+            assert(len(sents) == len(wordEs))
+            len_sents = torch.FloatTensor([len(li) for li in sents]) # get length of each sentense (batch_size)
+            sents = [self._insert(li, 'BOS') for li in sents] # add BOS sign
+            sents = [self._append(li, 'EOS') for li in sents] # add EOS sign
+            freq_sents = [map(lambda x: self.dict.get_freq_of_word(x), sent) for sent in sents] # get the freq
+            freq_sents = [torch.FloatTensor(li) for li in freq_sents]
+            #freq_sents.append(torch.FloatTensor(seq_len)) # should be the max length
+            padded_freq_sents = self._pad_sequence(freq_sents).unsqueeze(1) # (batch_size, 1, seq_len)
+            pref = self.alpha/(self.alpha + padded_freq_sents)
+            bmm = torch.bmm(pref, wordEs).squeeze()
+            for index in range(bmm.shape[0]):
+                bmm[index] = bmm[index] * (1.0/len_sents[index])
+            unit.append(bmm)
+        unit = torch.cat(unit, 0) # (batch_size, num_dim)
+        # pca
+        unit = unit.numpy()
+        pca = PCA(unit.shape[1], svd_solver = 'auto')
+        pca.fit(unit)
+        out = unit - np.dot(np.dot(unit, pca.components_.T), pca.components_)
+        print out.shape
+        return out
+
+    def _append(self, li, string):
+        tmp = li
+        tmp.append(string)
+        return tmp
+
+    def _insert(self, li, string):
+        tmp = li
+        tmp.insert(0, string)
+        return tmp
+
+    def _pad_sequence(self, data, max_length = 100):
+        PAD = 1
+        max_length = max_length
+        out = data[0].new(len(data), max_length).fill_(PAD)
+        for i in range(len(data)):
+            data_length = data[i].size(0)
+            offset = 0
+            out[i].narrow(0, offset, data_length).copy_(data[i])
+        return out
+
---
+++ utils/wordDict.py
@@ -0,0 +1,90 @@
+#-*- coding: utf-8 -*-
+from tqdm import tqdm
+import torch
+
+class WordDict:
+    def __init__(self, path, add_signs = True, resume = True, save_dicts = False):
+        """
+        the data used here should already be cleaned,
+        path: string or list of string
+            path of the dataset or the sentence list 
+        """
+        if resume:
+            print('-- loading dicts from the checkpoint  wordDict')
+            checkpoint = torch.load('./checkpoints/wordDict')
+            self.sents = checkpoint['sents']
+            self.dict = checkpoint['num_dict']
+            self.freq_dict = checkpoint['freq_dict']
+            self.num_of_words = len(self.dict)
+            print('-- finish')
+        else:
+            print('-- init word dict')
+            self.dict = {}
+            if type(path) == str:
+                self.sents = [li.rstrip('\n') for li in open(path)]
+            else:
+                self.sents = path
+            print('num of sentences: %d'%(len(self.sents)))
+            self.dict = self._get_num()
+            if add_signs:
+                self.dict['BOS'] = len(self.sents)
+                self.dict['EOS'] = len(self.sents)
+            self.num_of_words = len(self.dict)
+            print('num of words: %d'%(self.num_of_words))
+            self.freq_dict = self._get_freq()
+            print('-- finish')
+        if save_dicts:
+            self.save_dicts()
+
+    def get_freq_dict(self):
+        return self.freq_dict
+
+    def get_freq_of_word(self, word):
+        if word not in self.freq_dict:
+            return 0
+        else:
+            return self.freq_dict[word]
+    
+    def get_freq_of_words(self, words):
+        """
+        words is list of word
+        """
+        out = []
+        for word in words:
+            out.append(self.get_freq_of_word(word))
+        return out
+
+    def get_num_dict(self):
+        return self.dict
+
+    def get_num_of_word(self, word):
+        if word not in self.dict:
+            return 0
+        else:
+            return self.dict[word]
+
+    def save_dicts(self):
+        print('-- saving the dicts')
+        torch.save({
+            'sents': self.sents,
+            'num_dict': self.dict,
+            'freq_dict': self.freq_dict
+            }, './checkpoints/wordDict')
+        print('-- finish')
+
+    def _get_freq(self):
+        tmp = self.dict.copy()
+        for key in self.dict.keys():
+            tmp[key] = tmp[key]*.1/len(self.sents)
+        return tmp
+
+    def _get_num(self):
+        tmp = self.dict.copy()
+        for sent in tqdm(self.sents):
+            words = sent.split(' ')
+            for word in words:
+                if word in tmp.keys():
+                    tmp[word] += 1
+                else:
+                    tmp[word] = 1
+        return tmp
